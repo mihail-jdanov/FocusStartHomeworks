81838e6e4c5ff7764e3bd9e937712e1328b1c6a1
Михаил Жданов <mikhail.zhdanov@digital-ecosystems.ru>
2020-10-15 23:59:28 +0700

    Добавил структуру Car и enum Body

---
+++ Homework/Car Model/Car.swift
@@ -0,0 +1,19 @@
+//
+//  Car.swift
+//  Homework
+//
+//  Created by Михаил Жданов on 15.10.2020.
+//  Copyright © 2020 Михаил Жданов. All rights reserved.
+//
+
+import Foundation
+
+struct Car {
+    
+    var manufacturer: String
+    var model: String
+    var body: Body
+    var yearOfIssue: String?
+    var carNumber: String?
+    
+}
