e86c0d413bddd9e6452f72566e3dc3dce1814c41
Михаил Жданов <mikhail.zhdanov@digital-ecosystems.ru>
2020-10-15 23:19:33 +0700

    Создал новый проект

---
+++ Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by Михаил Жданов on 15.10.2020.
+//  Copyright © 2020 Михаил Жданов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
