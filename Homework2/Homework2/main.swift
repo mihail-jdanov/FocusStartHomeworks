7d8d21d5720d4270b4bca0653ba3ebe8e2e70bc1
Михаил Жданов <mikhail.zhdanov@digital-ecosystems.ru>
2020-10-25 20:36:32 +0700

    Создал workspace и поместил в него проект первой дом. работы, добавил проект второй работы

---
+++ Homework2/Homework2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Homework2
+//
+//  Created by Михаил Жданов on 25.10.2020.
+//  Copyright © 2020 Михаил Жданов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
