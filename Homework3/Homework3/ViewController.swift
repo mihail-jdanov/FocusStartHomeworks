8df2251c3e820d7153c18c8fbbf520cbb080586a
Михаил Жданов <mikhail.zhdanov@digital-ecosystems.ru>
2020-11-02 00:27:07 +0700

    Создал и добавил в workspace проект 3-й домашней работы

---
+++ Homework3/Homework3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework3
+//
+//  Created by Михаил Жданов on 02.11.2020.
+//  Copyright © 2020 Михаил Жданов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
